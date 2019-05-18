c918ca3a81ea2f6bea788ade981b7d08f429f65e
Libertad Chapinal <libertadchapinalcervantes@gmail.com>
2019-05-18 19:12:01 +0200

    Some advantages with conditional

--- src/App.js
+++ src/App.js
@@ -41,12 +41,44 @@ class App extends React.Component {
    return (
      <div className="App">
       <h1 className="title">LinkeDon</h1>
-      <input type="checkbox" id="male" className="gender__male" onClick={this.handleClickGender}/>Hombre
-      <input type="checkbox" id="female" className="gender__female" onClick={this.handleClickGender}/>mujer
+      <input type="checkbox" 
+      id="male" 
+      className="gender__male" 
+      onClick={this.handleClickGender}
+      //checked={this.state.isMan}
+      />Hombre
+      <input type="checkbox" 
+      id="female" 
+      className="gender__female" 
+      onClick={this.handleClickGender}
+      //checked={this.state.isWoman}
+      />Mujer
     
       <ul>
         {this.state.data
-        .filter(obj => obj.gender === 'male')
+        .filter(obj => {
+          return(
+
+            //Esto no pinta na
+            // this.state.isMan === true && this.state.isMan === true 
+            // ? obj.gender === 'female' && obj.gender === 'male' 
+            //   : this.state.isMan === true 
+            //   ? obj.gender === 'male' 
+            //     : this.state.isWoman === true
+            //     ? obj.gender === 'female'
+            //         : ''
+
+
+            //Este tocho de condicional no pinta a los hombres y mujeres
+            // this.state.isMan === true 
+            // ? obj.gender === 'male' 
+            // : this.state.isWoman === true 
+            // ? obj.gender === 'female' 
+            // : this.state.isWoman === true && this.state.isMan === true 
+            // ? obj.gender === 'female' && obj.gender === 'male'
+            // : ''
+
+        )})
         .map(item =>
            <li>
              <div className="card_container">
